990dc8deece95562dbb0788e35e7883695b72c11
JSR <csrday1004@gmail.com>
2024-04-16 22:23:58 +0900

    데이터.txt

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Alert } from 'bootstrap';
-import { useGameListQuery } from '../../hooks/apis/useGameList.js';
+// import { Alert } from 'bootstrap';
+// import { useGameListQuery } from '../../hooks/apis/useGameList.js';
 
 const DetailPage = () => {
 
-  const { data, isLoading, isError, error } = useGameListQuery()
+  // const { data, isLoading, isError, error } = useGameListQuery()
 
-  if (isLoading) {
-    return;
-  }
-  if (isError) {
-    return <Alert variant="danger">{error.message}</Alert>;
-  }
-  console.log(data)
+  // if (isLoading) {
+  //   return;
+  // }
+  // if (isError) {
+  //   return <Alert variant="danger">{error.message}</Alert>;
+  // }
+  // console.log(data)
 
   return <div>DetailPage Test2</div>;
 };
