6ecbdbdb8af27f0ab1063314adbc39381c2c2c44
DESKTOP-DM6TCSG\LeeJaeYong <agratos318@naver.com>
2024-04-16 23:45:04 +0900

    로그인 페이지 기본 컨테이너 작성

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -3,16 +3,15 @@ import { Alert } from 'bootstrap';
 import { useGameListQuery } from '../../hooks/apis/useGameList.js';
 
 const DetailPage = () => {
-
-  const { data, isLoading, isError, error } = useGameListQuery()
+  const { data, isLoading, isError, error } = useGameListQuery();
 
   if (isLoading) {
     return;
   }
   if (isError) {
-    return <Alert variant="danger">{error.message}</Alert>;
+    return <Alert variant='danger'>{error.message}</Alert>;
   }
-  console.log(data)
+  console.log(data);
 
   return <div>DetailPage Test2</div>;
 };
