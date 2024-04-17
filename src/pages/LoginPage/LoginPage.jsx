b83b53313850aaef45ef8e8b18eb0b9288e7b0d9
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2024-04-17 09:32:09 +0900

    LoginForm SocialLogin 작업중

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 
+import LoginForm from './components/LoginForm/LoginForm';
+import SocialLogin from './components/SocialLogin/SocialLogin';
+
 const LoginPage = () => {
   return (
     <Container>
       <Row>
-        <Col lg={6}>{/* //TODO - Login Form */}</Col>
-        <Col lg={6}>{/* //TODO - Social Login */}</Col>
+        <Col lg={6}>
+          {/* //TODO - Login Form */}
+          <LoginForm />
+        </Col>
+        <Col lg={6}>
+          {/* //TODO - Social Login */}
+          <SocialLogin />
+        </Col>
       </Row>
     </Container>
   );
