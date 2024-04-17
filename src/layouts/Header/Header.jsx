80d29c105f213ee8fdd32d820c4395edafccfff4
dongcheon1020 <dongcheon1020@gmail.com>
2024-04-17 22:24:57 +0900

    feat: search - search Input games

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { TiDelete } from 'react-icons/ti';
 
@@ -7,8 +7,10 @@ import logoImg from './img/logo.svg';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { searchActions } from '../../store/slice/searchValueSlice';
+import { useGameListQuery } from '../../hooks/apis/useGameList';
 
 const Header = () => {
+  const [searchGames, setSearchGames] = useState(0);
   const searchValue = useSelector((state) => state.search.searchValue);
   const dispatch = useDispatch();
 
@@ -16,6 +18,23 @@ const Header = () => {
     e.preventDefault();
     dispatch(searchActions.search(''));
   };
+
+  const { data } = useGameListQuery();
+  useEffect(() => {
+    setSearchGames(data.count);
+  }, [data]);
+
+  // 3의배수 , 찍어주는 함수
+  function formatNumberWithCommas(number) {
+    // 숫자를 문자열로 변환
+    const str = number.toString();
+    // 문자열을 역순으로 변환
+    const reversedStr = str.split('').reverse().join('');
+    // 3자리마다 쉼표 추가
+    const withCommas = reversedStr.match(/.{1,3}/g).join(',');
+    // 최종 문자열을 다시 역순으로 변환하여 반환
+    return withCommas.split('').reverse().join('');
+  }
 
   return (
     <div className='header-container'>
@@ -28,7 +47,7 @@ const Header = () => {
         <input
           id='header-search-input'
           type='text'
-          placeholder='Search'
+          placeholder={`Search  ${formatNumberWithCommas(searchGames)} games`}
           value={searchValue}
           onChange={(e) => dispatch(searchActions.search(e.target.value))}
         />
