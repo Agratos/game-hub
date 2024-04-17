13676a9be1a1da455745bbf2e3572d15b62c7a0a
김세현 <allezvvell@gmail.com>
2024-04-17 16:30:50 +0900

    슬라이더 작업중1

--- src/pages/TopGamePage/TopGamePage.jsx
+++ src/pages/TopGamePage/TopGamePage.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import TopGameCard from './component/TopGameCard/TopGameCard';
+import axios from 'axios';
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import TopGameCard from './component/TopGameCard/TopGameCard';
-import { useTopGameListQuery } from '../../hooks/apis/useTopGameList';
 
 const TopGamePage = () => {
-  const { data, isLoading, isError, error } = useTopGameListQuery();
+  const [allGameList, setAllGameList] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  let topGameList = [];
+  const getTopGameList = async () => {
+    setIsLoading(true);
+    try {
+      for (let i = 1; i <= 4; i++) {
+        const res = await axios.get('https://api.rawg.io/api/games', {
+          params: {
+            page: i,
+            page_size: 25,
+            key: process.env.REACT_APP_GAME_API,
+          },
+        });
+        topGameList = [...topGameList, ...res.data.results];
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+    setAllGameList(topGameList);
+  };
+
+  useEffect(() => {
+    getTopGameList();
+  }, []);
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  if (isError) {
-    return <div>{error.message}</div>;
-  }
   return (
-    <Swiper spaceBetween={30} slidesPerView={3} centeredSlides={true}>
-      {data.results.map((game, index) => (
+    <Swiper centeredSlides={true} slidesPerView={'auto'} spaceBetween={20}>
+      {allGameList?.map((game, index) => (
         <SwiperSlide key={index}>
           <TopGameCard game={game} />
         </SwiperSlide>
--- src/pages/TopGamePage/component/TopGameCard/TopGameCard.jsx
+++ src/pages/TopGamePage/component/TopGameCard/TopGameCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './TopGameCard.style.css';
 
 const TopGameCard = ({ game }) => {
   console.log('card', game);
-  return <div></div>;
+  return (
+    <div className='top-game-card'>
+      <div
+        className='img-box'
+        style={{ backgroundImage: `url(${game.background_image})` }}
+      ></div>
+      <div className='desc-box'>
+        <h3>{game.name}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default TopGameCard;
