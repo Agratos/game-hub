b7ad5b357cf46613b8eb897c1587976990a472df
JSR <csrday1004@gmail.com>
2024-04-17 21:00:20 +0900

    이미지넣기

---
+++ src/hooks/apis/useGameTrailer.js
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../../utils/api';
+
+// https://api.rawg.io/docs/#operation/games_movies_read
+// 제가 뭘 잘못한 건지 데이터를 못 가져오네요
+
+export const useGameTrailerQuery = ({ id }) => {
+  console.log(id);
+  const getGameTrailer = ({ id }) => {
+    return api.get(`/games/${id}/movies`);
+  };
+
+  return useQuery({
+    queryKey: ['game-trailer', id],
+    queryFn: () => getGameTrailer({ id }),
+    select: (res) => res.data,
+    retry: false,
+  });
+};
--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
+import { data } from './data';
+import './DetailPage.style.css';
+// import { useGameTrailerQuery } from '../../hooks/apis/useGameTrailer';
 // import { Alert } from 'bootstrap';
 // import { useGameListQuery } from '../../hooks/apis/useGameList.js';
 
 const DetailPage = () => {
+  // const { trailerData, isLoading } = useGameTrailerQuery({ id: 3328 });
+  // console.log(isLoading);
+  // console.log('트레일러데이터', trailerData);
 
-  // const { data, isLoading, isError, error } = useGameListQuery()
+  const fakeData = data.results[0];
+  // console.log(fakeData.background_image);
 
-  // if (isLoading) {
-  //   return;
-  // }
-  // if (isError) {
-  //   return <Alert variant="danger">{error.message}</Alert>;
-  // }
-  // console.log(data)
-
-  return <div>DetailPage Test2</div>;
+  return (
+    <div className='detail-bg'>
+      <div className='detail-section-1'>
+        <div className='detail-section-1-1'>
+          <div className='detail-media-container'>
+            {fakeData.short_screenshots?.map((e, index) => {
+              console.log('ddd', e.image);
+              return (
+                <div className='detail-media-item-box' key={index}>
+                  <div
+                    className='detail-media-item'
+                    style={{ backgroundImage: `url(${e.image})` }}
+                  >
+                    <div className='detail-media-retio'></div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className='detail-section-1-2'>
+          <div className='datail-name'>{fakeData.name}</div>
+        </div>
+      </div>
+      <div className='detail-section-2'></div>
+    </div>
+  );
 };
 
 export default DetailPage;
