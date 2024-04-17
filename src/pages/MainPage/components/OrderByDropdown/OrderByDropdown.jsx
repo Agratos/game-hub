ec67f2c7c86235d08034874f2e3f996897f3a11c
raon9401 <raon9401@naver.com>
2024-04-18 00:09:16 +0900

    Style: MainPage style dropdown button add

---
+++ src/pages/MainPage/components/OrderByDropdown/OrderByDropdown.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Dropdown } from 'react-bootstrap'
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+const OrderByDropdown = ({ORDER_ARR}) => {
+    // console.log(ORDER_ARR);
+  const navigate = useNavigate();
+  const [query,] = useSearchParams();
+  
+  const handleFirstCharUpper = (item) => {
+    return item[0].toUpperCase() + item.slice(1).toLowerCase();
+  }
+
+  const goOrdering = (item) => {
+    navigate(`/?ordering=${item}`);
+  }
+
+
+  return (
+    <Dropdown>
+    <Dropdown.Toggle variant="dark" id="dropdown-basic" className='p-1' >
+    <span style={{fontSize: "0.8rem"}}>Order By: {query.get("ordering") ? handleFirstCharUpper(query.get("ordering")) : handleFirstCharUpper(ORDER_ARR[0])}</span>
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu>
+    {ORDER_ARR.map((item, index) => 
+      <Dropdown.Item onClick={() => goOrdering(item)} key={index}>{handleFirstCharUpper(item)}</Dropdown.Item>
+    )}
+    </Dropdown.Menu>
+  </Dropdown>
+  )
+}
+
+export default OrderByDropdown
