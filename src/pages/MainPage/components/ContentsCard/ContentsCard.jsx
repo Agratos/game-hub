6f5e2e2807115feed71fa6ebf11ce7c2c95f85a4
raon9401 <raon9401@naver.com>
2024-04-19 00:17:22 +0900

    Feat: main card hover event feat

--- src/pages/MainPage/components/ContentsCard/ContentsCard.jsx
+++ src/pages/MainPage/components/ContentsCard/ContentsCard.jsx
@@ -1,26 +1,40 @@
+// import { Card } from "react-bootstrap";
+import "./ContentsCard.style.css"
 import React from 'react'
-import { Card } from 'react-bootstrap'
 
 const exceptContents = [1081, 50];
 
 const ContentsCard = ({item}) => {
 // 1081, 50 tags 는 제외 시키기.
 
+
   const handleImage = () => {
     // console.log(item?.tags.filter((el) => exceptContents.includes(el.id)).length === 0 && item?.background_image);
     return item?.tags.filter((el) => exceptContents.includes(el.id)).length === 0 && item?.background_image;
   }
 
+
+
   return (
-    <Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" style={{height:'10rem'}} src={handleImage() ? item?.background_image : "/images/emptyImage.webp"} />
-      <Card.Body>
-        <Card.Title>{item?.name}</Card.Title>
-        <Card.Text>
-          
-        </Card.Text>
-      </Card.Body>
-    </Card>
+      <figure className='mainpage-card-area'style={{ width: '18rem'}}>
+          <img style={{height:'10rem'}} src={handleImage() ? item?.background_image : "/images/emptyImage.webp"} alt="card img"/>
+          <figcaption>
+            <h4>
+              {item?.name}
+            </h4>
+            <div className="mainpage-card-overlay">
+              <div>
+                dddddd
+              </div>
+              <div>
+                dddddd
+              </div>
+              <div>
+                dddddd
+              </div>
+            </div>
+          </figcaption>
+      </figure>
   )
 }
 
