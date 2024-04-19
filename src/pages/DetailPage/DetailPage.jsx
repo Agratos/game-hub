6206b6db8a7ca26417b05bd18d2edd484fb2c323
JSR <csrday1004@gmail.com>
2024-04-19 18:19:02 +0900

    스핀로더

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,131 +1,172 @@
-import React from 'react';
-import { data } from './data';
+import React, { useState } from 'react';
+// import { data } from './data';
 import './DetailPage.style.css';
 import { useGameScreenShotsQuery } from '../../hooks/apis/useGameScreenShots';
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 import { IoAddCircle } from 'react-icons/io5';
-// import { useGameTrailerQuery } from '../../hooks/apis/useGameTrailer';
-// import { Alert } from 'bootstrap';
-// import { useGameListQuery } from '../../hooks/apis/useGameList.js';
+import { useGameTrailerQuery } from '../../hooks/apis/useGameTrailer';
+import { FaBan, FaMeh, FaRegGrinHearts, FaRegThumbsUp } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
+import { useGameDetailQuery } from '../../hooks/apis/useGameDetail.js';
 
 const DetailPage = () => {
-  // const { data: trailerData, isLoading } = useGameTrailerQuery({ id: 3498 });
-  // console.log(isLoading);
-  // console.log('트레일러데이터', trailerData);
-  //
-  const bgColor = ['green', 'blue', 'yellow', 'red'];
-  const { data: screenShotsData } = useGameScreenShotsQuery({ game_pk: 3328 });
-  console.log('스크린샷불러온거 :', screenShotsData);
-  console.log('스크린샷불러온거 :', screenShotsData);
+  const { id } = useParams();
+  console.log(id);
 
-  const trailerData = [
-    {
-      results: {
-        data: {
-          480: 'https://steamcdn-a.akamaihd.net/steam/apps/256693661/movie480.mp4',
-          max: 'https://steamcdn-a.akamaihd.net/steam/apps/256693661/movie_max.mp4',
-        },
-      },
-    },
+  const { data: detailData, isLoading: detailLoading } = useGameDetailQuery({
+    id,
+  });
+  console.log('디테일데이터', detailData);
+  // const detailData = data;
+  //위 두개 중 하나만 쓰기
+
+  const [viewAllAbout, setViewAllAbout] = useState(false);
+  const { data: trailerData, isLoading: trailerLoading } = useGameTrailerQuery({
+    id,
+  });
+  console.log('트레일러데이터', trailerData?.results);
+
+  const bgColor = ['green', 'blue', 'yellow', 'red'];
+  const voteIcon = [
+    <FaRegGrinHearts key={'FaRegGrinHearts'} />,
+    <FaRegThumbsUp key={'FaRegThumbsUp'} />,
+    <FaMeh key={'FaMeh'} />,
+    <FaBan key={'FaBan'} />,
   ];
 
-  console.log('트레일러영상 링크 :', trailerData[0].results.data.max);
+  const { data: screenShotsData, isLoading: screenShotLoading } =
+    useGameScreenShotsQuery({ game_pk: id });
+  console.log('스크린샷불러온거 :', screenShotsData?.results);
 
-  const fakeData = data.results[1];
-  console.log(fakeData.slug);
-
-  return (
-    <div className='detail-bg'>
-      <div className='detail-section-1'>
-        <div className='detail-section-1-1'>
-          <div className='detail-media-container'>
-            {fakeData.short_screenshots?.map((e, index) => {
-              return (
-                <div className='detail-media-item-box' key={index}>
-                  {fakeData.short_screenshots.length - 1 === index ? (
-                    <div
-                      className='detail-media-item '
-                      style={{ backgroundImage: `url(${e.image})` }}
-                    >
-                      <div className='detail-media-item-overay'>more</div>
+  if (!detailLoading && !trailerLoading && !screenShotLoading) {
+    return (
+      <div className='detail-bg'>
+        <div className='detail-section-1'>
+          <div className='detail-section-1-1'>
+            <div className='detail-media-container'>
+              {screenShotsData?.results.map((e, index) => {
+                if (index > 3) {
+                  return;
+                } else {
+                  return (
+                    <div className='detail-media-item-box' key={index}>
+                      {index === 3 ? (
+                        <div
+                          className='detail-media-item '
+                          style={{ backgroundImage: `url(${e.image})` }}
+                        >
+                          <div className='detail-media-item-overay'>more</div>
+                        </div>
+                      ) : (
+                        <div
+                          className='detail-media-item'
+                          style={{ backgroundImage: `url(${e.image})` }}
+                        />
+                      )}
                     </div>
-                  ) : (
-                    <div
-                      className='detail-media-item'
-                      style={{ backgroundImage: `url(${e.image})` }}
-                    />
-                  )}
-                </div>
-              );
-            })}
+                  );
+                }
+              })}
+            </div>
           </div>
-        </div>
 
-        <div className='detail-section-1-2'>
-          <div className='datail-name'>{fakeData.name}</div>
-          <div className='detail-rank-add'>
-            <div className='detail-rank'>
-              {fakeData.rating}({fakeData.ratings_count})
-              <span> {fakeData.rating_top.toLocaleString()} RATINGS</span>
-            </div>
-            <div className='detail-add'>
-              <Button
-                variant='light'
-                style={{
-                  textAlign: 'left',
-                  width: '150px',
-                  position: 'relative',
-                  overflow: 'hidden',
-                  border: 'none',
-                }}
-              >
-                <p style={{ fontSize: '10px', color: 'gray' }}>Add to</p>
-                <p>My games</p>
-                <IoAddCircle
+          <div className='detail-section-1-2'>
+            <div className='datail-name'>{detailData?.name}</div>
+            <div className='detail-rank-add'>
+              <div className='detail-rank'>
+                <div>
+                  {
+                    detailData?.ratings.find(
+                      (e) => e.id === detailData?.rating_top
+                    ).title
+                  }
+                </div>
+                <h6>
+                  {detailData?.rating} / 5 point{' '}
+                  <span style={{ color: 'gray' }}>
+                    ({detailData?.ratings_count.toLocaleString()} votes)
+                  </span>
+                </h6>
+              </div>
+              <div className='detail-add'>
+                <Button
+                  variant='light'
                   style={{
-                    position: 'absolute',
-                    top: '0px',
-                    right: '-12px',
-                    width: '50px',
-                    height: '50px',
-                    color: 'green',
+                    textAlign: 'left',
+                    width: '150px',
+                    position: 'relative',
+                    overflow: 'hidden',
+                    border: 'none',
                   }}
-                />
-              </Button>
+                >
+                  <p style={{ fontSize: '10px', color: 'gray' }}>Add to</p>
+                  <p>My games</p>
+                  <IoAddCircle
+                    style={{
+                      position: 'absolute',
+                      top: '0px',
+                      right: '-12px',
+                      width: '50px',
+                      height: '50px',
+                      color: 'green',
+                    }}
+                  />
+                </Button>
+              </div>
             </div>
-          </div>
-          <div className='detail-rating-bar'>
-            {fakeData.ratings.map((e, index) => {
-              return (
-                <div
-                  style={{
-                    width: `${e.percent}%`,
-                    background: `${bgColor[index]}`,
-                  }}
-                  key={index}
-                ></div>
-              );
-            })}
-          </div>
-          <div className='detail-rating-count'>
-            {fakeData.ratings.map((e, index) => {
-              return (
-                <button className='detail-rating-btn' key={index}>
+            <div className='detail-rating-bar'>
+              {detailData?.ratings.map((e, index) => {
+                return (
                   <div
-                    className='detail-rating-color'
-                    style={{ background: `${bgColor[index]}` }}
+                    style={{
+                      width: `${e.percent}%`,
+                      background: `${bgColor[index]}`,
+                    }}
+                    key={index}
                   ></div>
-                  <div style={{ fontWeight: 'bold' }}>{e.title}</div>
-                  <div style={{ color: 'gray' }}>{e.count}</div>
-                </button>
-              );
-            })}
+                );
+              })}
+            </div>
+            <div className='detail-rating-count'>
+              {detailData?.ratings.map((e, index) => {
+                return (
+                  <button className='detail-rating-btn' key={index}>
+                    <div
+                      className='detail-rating-color'
+                      style={{ color: `${bgColor[index]}` }}
+                    >
+                      {voteIcon[index]}
+                    </div>
+                    <div style={{ fontWeight: 'bold' }}>{e.title}</div>
+                    <div style={{ color: 'gray' }}>
+                      {e.count.toLocaleString()}
+                    </div>
+                  </button>
+                );
+              })}
+            </div>
+            <div className='detail-about'>
+              <h4>About</h4>
+              {viewAllAbout
+                ? detailData?.description.replace(/<[^>]*>?/gm, '')
+                : detailData?.description
+                    .replace(/<[^>]*>?/gm, '')
+                    .slice(0, 300) + '...'}
+              <button
+                className='detail-about-more-btn'
+                onClick={() => setViewAllAbout(!viewAllAbout)}
+              >
+                {!viewAllAbout ? '▼Read more' : '▲Show less'}
+              </button>
+            </div>
           </div>
         </div>
+        <div className='detail-section-2'></div>
       </div>
-      <div className='detail-section-2'></div>
-    </div>
-  );
+    );
+  } else {
+    return <Spinner />;
+  }
 };
 
 export default DetailPage;
