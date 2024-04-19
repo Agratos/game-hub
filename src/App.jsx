3e1325e8f1c4cae4ba2c84f16d7110987a0380a9
dongcheon1020 <dongcheon1020@gmail.com>
2024-04-19 22:34:58 +0900

    변경 사항 적용 및 구조 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import AppLayout from './layouts/AppLayout';
+import AppLayout from './layouts/AppLayoout/Layout.jsx/AppLayout';
 import MainPage from './pages/MainPage/MainPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import DetailPage from './pages/DetailPage/DetailPage';
 import SearchPage from './pages/SearchPage/SearchPage';
 import TopGamePage from './pages/TopGamePage/TopGamePage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import HeaderLayout from './layouts/AppLayoout/HeaderLayout/HeaderLayout';
 
 const App = () => {
   return (
     <div>
-      <Routes>
+      {/* <Routes>
         <Route path='/' element={<AppLayout />}>
           <Route index element={<MainPage />} />
           <Route path='detail/:id' element={<DetailPage />} />
@@ -20,6 +21,18 @@ const App = () => {
           <Route path='top-game' element={<TopGamePage />} />
         </Route>
         <Route path='/login' element={<LoginPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes> */}
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path='detail/:id' element={<DetailPage />} />
+          <Route path='search' element={<SearchPage />} />
+        </Route>
+        <Route element={<HeaderLayout />}>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/top-game' element={<TopGamePage />} />
+        </Route>
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </div>
---
+++ src/layouts/AppLayoout/HeaderLayout/HeaderLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../../component/Header/Header';
+import './HeaderLayout.style.css';
+// import { useSelector } from 'react-redux';
+
+const HeaderLayout = () => {
+  //   const hamburgerOn = useSelector((state) => state.hamburger.hamburgerOn);
+  return (
+    <div className='applayout-wrapper'>
+      <header className='applayout-header'>
+        <Header />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default HeaderLayout;
