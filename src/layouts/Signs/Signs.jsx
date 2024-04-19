c80016dbb52b24939ec39b9be859b7a08aa7cc45
dongcheon1020 <dongcheon1020@gmail.com>
2024-04-19 18:03:12 +0900

    feat: 로그인 프로필 추가

--- src/layouts/Signs/Signs.jsx
+++ src/layouts/Signs/Signs.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Signs.style.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { authenicateActions } from '../../store/slice/authenicateSlice';
+import { IoPersonCircleOutline } from 'react-icons/io5';
 
 const Signs = () => {
+  const dispatch = useDispatch();
+  const { id, profileImage, authenticate } = useSelector((state) => state.auth);
+  console.log('13123123123', id, profileImage, authenticate);
+
   return (
     <>
-      <li className='sign-item'>
-        <Link to='/login'>
-          <span className='header-sign-span'>Sign in</span>
-        </Link>
-      </li>
-      <li className='sign-item'>
-        <Link to='/login'>
-          <span className='header-sign-span'>Sign up</span>
-        </Link>
-      </li>
+      {authenticate ? (
+        <>
+          <li>
+            <button
+              className='sign-logout-bt'
+              onClick={() => dispatch(authenicateActions.logout())}
+            >
+              Sign out
+            </button>
+          </li>
+          <li className='sign-item'>
+            <Link to='/mypage'>
+              <div className='Sign-profile'>
+                {profileImage === null ? (
+                  <IoPersonCircleOutline className='sign-default-img' />
+                ) : (
+                  <img src={profileImage} alt='profile-img' />
+                )}
+              </div>
+            </Link>
+          </li>
+        </>
+      ) : (
+        <li className='sign-item'>
+          <Link to='/login'>
+            <span className='header-sign-span'>Sign in</span>
+          </Link>
+        </li>
+      )}
     </>
   );
 };
