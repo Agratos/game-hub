ca349b7ab0f1f2badc6724ee07a2f5ff65f36328
dongcheon1020 <dongcheon1020@gmail.com>
2024-04-20 19:14:46 +0900

    docs: Mypage

---
+++ src/pages/MyPage/MyPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MyPage = () => {
+  return (
+    <div
+      style={{
+        color: '#fff',
+      }}
+    >
+      mypage
+    </div>
+  );
+};
+
+export default MyPage;
