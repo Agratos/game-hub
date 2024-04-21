4368d079ebe5f69278a134728915f689ec73bde2
DESKTOP-DM6TCSG\LeeJaeYong <agratos318@naver.com>
2024-04-21 15:57:11 +0900

    persist 추가

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
 import App from './App';
 import store from './store/store';
+import './index.css';
 
 const queryClient = new QueryClient();
+const persistedReducer = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
