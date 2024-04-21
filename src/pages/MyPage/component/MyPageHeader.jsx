220252ac351c73cf4b585f05e7c3b099d9b02d30
dongcheon1020 <dongcheon1020@gmail.com>
2024-04-21 17:07:27 +0900

    feat: sign out navigate

--- src/pages/MyPage/component/MyPageHeader.jsx
+++ src/pages/MyPage/component/MyPageHeader.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { authenicateActions } from '../../../store/slice/authenicateSlice';
+import { useNavigate } from 'react-router-dom';
 
 const MyPageHeader = ({ userName }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signs = () => {
+    dispatch(authenicateActions.logout());
+    navigate('/');
+    alert('로그아웃됨');
+  };
   return (
     <>
       <article className='mypage-header-title'>
         <h3>계정</h3>
-        <button onClick={() => dispatch(authenicateActions.logout())}>
+        <button onClick={() => signs()}>
           <span>Sign out</span>
         </button>
       </article>
